d6aa6b8d73b247721b1b69c616e9c1fc9cf851a8
Derek Vogel <78238679+Dvogel345@users.noreply.github.com>
2021-11-20 09:45:20 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+// map object
+
+// Leaflet map
+	
+// add openstreetmap tiles
+	
+// create and add geolocation marker
+	
+// add business markers
+
+// get coordinates via geolocation api 
+
+// get foursquare businesses using REST
+
+// foursquare array
+
+// event handlers
+
+// window load
+
+// business submit button
