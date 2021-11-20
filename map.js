cc60c9bb5bf309089d608b6b83e7f3412397d953
Aaron Lu <aaronnoh0409@gmail.com>
2021-11-20 10:17:38 -0500

    if the user allows the access fot the current location, map will zoom into that location

--- map.js
+++ map.js
@@ -1,13 +1,13 @@
-async function getCoords(){
-    pos = await new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(resolve, reject)
-    })
-    return [pos.coords.latitude, pos.coords.longitude]
-}
+// async function getCoords(){
+//     pos = await new Promise((resolve, reject) => {
+//         navigator.geolocation.getCurrentPosition(resolve, reject)
+//     })
+//     return [pos.coords.latitude, pos.coords.longitude]
+// }
 
 
 // create map
-const myMap = L.map('map', {
+const map = L.map('map', {
     center: [40.0583, -74.4057],
     zoom: 12,
 });
@@ -15,12 +15,14 @@ const myMap = L.map('map', {
 // add openstreetmap tiles
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(myMap)
+}).addTo(map)
+map.locate({setView: true, maxZoom: 16});
+
 
 // create and main add geolocation marker
-window.onload = async () => {
-    const coords = await getCoords()
-    L.marker([coords[0], coords[1]])
-}
+// window.onload = async () => {
+//     const coords = await getCoords()
+//     L.marker([coords[0], coords[1]])
+// }
 // const marker = L.marker([40.0583, -74.4057])
 // marker.addTo(myMap).bindPopup('<p1><b>RANDOM LOCATION</b></p1>').openPopup()
