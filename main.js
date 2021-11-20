0cfec976384f28e74c8518868715c95aac59aebc
Derek Vogel <78238679+Dvogel345@users.noreply.github.com>
2021-11-20 10:03:44 -0500

    debugging

--- main.js
+++ main.js
@@ -1,18 +1,34 @@
-// map object
+// creating a map object
+const myMap = {
+	coordinates: [],
+	businesses: [],
+	map: {},
+	markers: {},
 
-// Leaflet map
-	
+// build leaflet map
+    buildMap() {
+        this.map = L.map('map', {
+        center: this.coordinates,
+        zoom: 10,
+        }),
 // add openstreetmap tiles
-	
-// create and add geolocation marker
-	
-// add business markers
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        minZoom: '15',
+        }).addTo(this.map)
+// create and add geolocation marker for self
+    const marker = L.marker(this.coordinates)
+    marker.addTo(this.map).bindPopup('<p1><b>Me</b><br></p1>').openPopup()
+    }
+}
+// adding business markers
 
 // get coordinates via geolocation api 
 
-// get foursquare businesses using REST
+// get foursquare businesses using REST GET method
 
-// foursquare array
+// processig the foursquare array
 
 // event handlers
 
