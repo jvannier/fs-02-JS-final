d6e6e3aff775495c259cab35226db3696adf5f31
Aaron Lu <aaronnoh0409@gmail.com>
2021-11-20 11:19:06 -0500

    geolocation plugin added to show the user's current location and pin the location (only if the user allows it)

--- map.js
+++ map.js
@@ -10,13 +10,15 @@
 const map = L.map('map', {
     center: [40.0583, -74.4057],
     zoom: 12,
-});
+})
 
 // add openstreetmap tiles
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map)
-map.locate({setView: true, maxZoom: 16});
+
+map.locate({setView: true, maxZoom: 16})
+L.control.locate().addTo(map)
 
 
 // create and main add geolocation marker
