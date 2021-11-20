fcfd2d912e1962022cfb60a1749999111fddcdf7
Derek Vogel <78238679+Dvogel345@users.noreply.github.com>
2021-11-20 14:59:50 -0500

    Done adusting names

--- main.js
+++ main.js
@@ -12,31 +12,29 @@ const myMap = {
         center: this.coordinates,
         zoom: 4,
         }),
-// add openstreetmap tiles
+    // add openstreetmap tiles
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution:
             '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         minZoom: '1',
         }).addTo(this.map)
 
-// // Personalized icon
-    let LeafIcon = L.Icon.extend({
-        options: {
+    // Personalized icon
+    let leafIcon = L.icon({
             iconUrl: 'leaf-green.png',
             iconSize:     [38, 95],
             shadowSize:   [50, 64],
             iconAnchor:   [22, 94],
             shadowAnchor: [4, 62],
             popupAnchor:  [-3, -76]
-        }
     }),
         
-// create and add geolocation marker for self
-    marker = L.marker(this.coordinates, {icon: LeafIcon})
+    // create and add geolocation marker for self
+    marker = L.marker(this.coordinates, {icon: leafIcon})
         marker.addTo(this.map).bindPopup('<p1><b>This is Me!</b><br></p1>').openPopup()
     },
 
-// adding business markers
+    // adding business markers
     addMarkers(businesses) {
         for (let i = 0; i < businesses.length; i++) {
             // console.log(businesses[i].lat)
@@ -50,28 +48,29 @@ const myMap = {
         }
     },
 }
-// get coordinates via geolocation api 
-async function getCoords(){
-	const position = await new Promise((resolve, reject) => {
-		navigator.geolocation.getCurrentPosition(resolve, reject)
-	});
-	return [position.coords.latitude, position.coords.longitude]
-}
-// changed to mock.api
-async function getFoursquare(business) {
-    const options = {
-    method: "GET",
-    headers: { 
-        "Content-Type": "application/json"
+    // get coordinates via geolocation api 
+    async function getCoords(){
+        const position = await new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition(resolve, reject)
+        });
+        return [position.coords.latitude, position.coords.longitude]
     }
-    };
-        let response = await fetch(`https://60d23844858b410017b2d60b.mockapi.io/places`, options)
-        let data = await response.text()
-        console.log(data)
-        let parsedData = JSON.parse(data)
-        let businesses = parsedData
-        return businesses
-}
+
+    // changed to mock.api
+    async function getMockapi(business) {
+        const options = {
+        method: "GET",
+        headers: { 
+            "Content-Type": "application/json"
+        }
+        };
+            let response = await fetch(`https://60d23844858b410017b2d60b.mockapi.io/places`, options)
+            let data = await response.text()
+            console.log(data)
+            let parsedData = JSON.parse(data)
+            let businesses = parsedData
+            return businesses
+    }
 // // processing data from api
 // function businessesData(data) {
 // 	// let businesses = data.map(() => {
@@ -110,7 +109,7 @@ document.getElementById('submit')
 addEventListener('click', async (event) => {
 	console.log("this is working");
 	business = document.getElementById('business').value
-	let data = await getFoursquare(business)		
+	let data = await getMockapi(business)		
 	event.preventDefault()
 	businessData(data)
 })
